e47c14427501b0272a21cc681c143487629461a0
TohirDev <isakulovtohir902@gmail.com>
2023-09-03 17:39:33 +0500

    DONE: displaying data

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
 const dice = document.querySelector(".dice");
+let adviceId = document.getElementById("id");
+let adviceText = document.getElementById("text");
 
 dice.addEventListener("click", () => {
   dice.classList.toggle("rotate");
+  fetchData();
+});
+
+const displayRandomAdvice = (data) => {
+  adviceId.innerText = `#${data.slip.id}`;
+  adviceText.innerText = data.slip.advice;
+};
+
+const fetchData = () => {
+  adviceId.innerText = "...";
+  adviceText.innerText = "Loading...";
   fetch("https://api.adviceslip.com/advice")
     .then((res) => res.json())
-    .then((data) => console.log(data));
-});
+    .then((data) => displayRandomAdvice(data));
+};
